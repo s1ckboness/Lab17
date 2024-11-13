f051feb9d9ea02cbd70eefd4d7137eca41ee71c3
s1ckboness <juandavidpenuelaperez24@gmail.com>
2024-11-12 20:06:31 -0500

    Lab17

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('welcomeBtn').addEventListener('click', function() {
+    alert('¡Bienvenido a mi portafolio!');
+});
+
+const form = document.getElementById('contactForm');
+form.addEventListener('submit', function(event) {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    if (name === '' || email === '') {
+        alert('Por favor, completa todos los campos.');
+        event.preventDefault();
+    } else {
+        alert('¡Formulario enviado correctamente!');
+    }
+});
